ec26208beeca25217c4dbea793a91e3acd06d4ac
Raghul-M <ragm@redhat.com>
2023-11-20 13:10:26 +0530

    Add unit tests for support package

--- support/environment_test.go
+++ support/environment_test.go
@@ -1,83 +1,82 @@
 package support
 
 import (
-    "os"
-    "testing"
+	"os"
+	"testing"
 )
 
 func TestGetCodeFlareSDKVersion(t *testing.T) {
-    // Set the environment variable.
-    os.Setenv(CodeFlareTestSdkVersion, "1.2.3")
+	// Set the environment variable.
+	os.Setenv(CodeFlareTestSdkVersion, "1.2.3")
 
-    // Get the version.
-    version := GetCodeFlareSDKVersion()
+	// Get the version.
+	version := GetCodeFlareSDKVersion()
 
-    // Assert that the version is correct.
-    if version != "1.2.3" {
-        t.Errorf("Expected version 1.2.3, but got %s", version)
-    }
+	// Assert that the version is correct.
+	if version != "1.2.3" {
+		t.Errorf("Expected version 1.2.3, but got %s", version)
+	}
 }
 
 func TestGetRayVersion(t *testing.T) {
-    // Set the environment variable.
-    os.Setenv(CodeFlareTestRayVersion, "1.4.5")
+	// Set the environment variable.
+	os.Setenv(CodeFlareTestRayVersion, "1.4.5")
 
-    // Get the version.
-    version := GetRayVersion()
+	// Get the version.
+	version := GetRayVersion()
 
-    // Assert that the version is correct.
-    if version != "1.4.5" {
-        t.Errorf("Expected version 1.4.5, but got %s", version)
-    }
+	// Assert that the version is correct.
+	if version != "1.4.5" {
+		t.Errorf("Expected version 1.4.5, but got %s", version)
+	}
 }
 
 func TestGetRayImage(t *testing.T) {
-    // Set the environment variable.
-    os.Setenv(CodeFlareTestRayImage, "ray/ray:latest")
+	// Set the environment variable.
+	os.Setenv(CodeFlareTestRayImage, "ray/ray:latest")
 
-    // Get the image.
-    image := GetRayImage()
+	// Get the image.
+	image := GetRayImage()
 
-    // Assert that the image is correct.
-    if image != "ray/ray:latest" {
-        t.Errorf("Expected image ray/ray:latest, but got %s", image)
-    }
+	// Assert that the image is correct.
+	if image != "ray/ray:latest" {
+		t.Errorf("Expected image ray/ray:latest, but got %s", image)
+	}
 }
 
 func TestGetPyTorchImage(t *testing.T) {
-    // Set the environment variable.
-    os.Setenv(CodeFlareTestPyTorchImage, "pytorch/pytorch:latest")
+	// Set the environment variable.
+	os.Setenv(CodeFlareTestPyTorchImage, "pytorch/pytorch:latest")
 
-    // Get the image.
-    image := GetPyTorchImage()
+	// Get the image.
+	image := GetPyTorchImage()
 
-    // Assert that the image is correct.
-    if image != "pytorch/pytorch:latest" {
-        t.Errorf("Expected image pytorch/pytorch:latest, but got %s", image)
-    }
+	// Assert that the image is correct.
+	if image != "pytorch/pytorch:latest" {
+		t.Errorf("Expected image pytorch/pytorch:latest, but got %s", image)
+	}
 }
 
-func TestGetClusterID(t *testing.T){
+func TestGetClusterID(t *testing.T) {
 	os.Setenv("OsdClusterID", "my-cluster-id")
-    clusterId, ok := GetOsdClusterId()
-    if !ok {
-        t.Errorf("Expected GetOsdClusterId() to return true, but got false.")
-    }
-    if clusterId != "my-cluster-id" {
-        t.Errorf("Expected GetOsdClusterId() to return 'my-cluster-id', but got '%s'.", clusterId)
-    }
+	clusterId, ok := GetOsdClusterId()
+	if !ok {
+		t.Errorf("Expected GetOsdClusterId() to return true, but got false.")
+	}
+	if clusterId != "my-cluster-id" {
+		t.Errorf("Expected GetOsdClusterId() to return 'my-cluster-id', but got '%s'.", clusterId)
+	}
 
-	
 }
-func TestGetInstascaleOcmSecret(t *testing.T){
+func TestGetInstascaleOcmSecret(t *testing.T) {
 	// Set the Instascale OCM secret environment variable.
 	os.Setenv(InstaScaleOcmSecret, "default/instascale-ocm-secret")
 	// Get the Instascale OCM secret namespace and secret name.
-namespace, secretName := GetInstascaleOcmSecret()
+	namespace, secretName := GetInstascaleOcmSecret()
 
-// Verify that the namespace and secret name are correct.
-if namespace != "default" || secretName != "instascale-ocm-secret" {
-    t.Errorf("Expected GetInstascaleOcmSecret() to return 'default/instascale-ocm-secret', but got '%s/%s'.", namespace, secretName)
+	// Verify that the namespace and secret name are correct.
+	if namespace != "default" || secretName != "instascale-ocm-secret" {
+		t.Errorf("Expected GetInstascaleOcmSecret() to return 'default/instascale-ocm-secret', but got '%s/%s'.", namespace, secretName)
+	}
+
 }
-
-}	
--- support/ray_test.go
+++ support/ray_test.go
@@ -1,45 +1,46 @@
 package support
 
 import (
-    "context"
-    "fmt"
-    "testing"
+	"context"
+	"fmt"
+	"testing"
 
-    "github.com/onsi/gomega"
-    rayv1alpha1 "github.com/ray-project/kuberay/ray-operator/apis/ray/v1"
-    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-    "k8s.io/apimachinery/pkg/runtime"
-    "sigs.k8s.io/controller-runtime/pkg/client"
-    "sigs.k8s.io/controller-runtime/pkg/client/fake"
+	"github.com/onsi/gomega"
+	rayv1alpha1 "github.com/ray-project/kuberay/ray-operator/apis/ray/v1"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 )
 
 func NewFakeClient(scheme *runtime.Scheme, objects ...client.Object) client.Client {
-    return fake.NewClientBuilder().WithScheme(scheme).WithObjects(objects...).Build()
+	return fake.NewClientBuilder().WithScheme(scheme).WithObjects(objects...).Build()
 }
 
 func TestGetRayJob(t *testing.T) {
-    g := gomega.NewGomegaWithT(t)
+	g := gomega.NewGomegaWithT(t)
 
-    scheme := runtime.NewScheme()
-    _ = rayv1alpha1.AddToScheme(scheme)
+	scheme := runtime.NewScheme()
+	_ = rayv1alpha1.AddToScheme(scheme)
 
-    fakeRayJobs := []client.Object{
-        &rayv1alpha1.RayJob{
-            ObjectMeta: metav1.ObjectMeta{
-                Name:      "my-job-1",
-                Namespace: "my-namespace",
-            },
-        },
-    }
+	fakeRayJobs := []client.Object{
+		&rayv1alpha1.RayJob{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "my-job-1",
+				Namespace: "my-namespace",
+			},
+		},
+	}
 
-    fakeClient := NewFakeClient(scheme, fakeRayJobs...)
+	fakeClient := NewFakeClient(scheme, fakeRayJobs...)
 
-    rayJob := &rayv1alpha1.RayJob{}
-    err := fakeClient.Get(context.TODO(), client.ObjectKey{Name: "my-job-1", Namespace: "my-namespace"}, rayJob)
-    g.Expect(err).ToNot(gomega.HaveOccurred())
+	rayJob := &rayv1alpha1.RayJob{}
+	err := fakeClient.Get(context.TODO(), client.ObjectKey{Name: "my-job-1", Namespace: "my-namespace"}, rayJob)
+	g.Expect(err).ToNot(gomega.HaveOccurred())
 
-    fmt.Printf("Retrieved job object: %+v\n", rayJob)
+	fmt.Printf("Retrieved job object: %+v\n", rayJob)
 
-    g.Expect(rayJob.Name).To(gomega.Equal("my-job-1"))
-    g.Expect(rayJob.Namespace).To(gomega.Equal("my-namespace"))
+	g.Expect(rayJob.Name).To(gomega.Equal("my-job-1"))
+	g.Expect(rayJob.Namespace).To(gomega.Equal("my-namespace"))
 }
