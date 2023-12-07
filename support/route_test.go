1801e8f3bf2f6e88ae0dd96fc4af1a157f942184
Raghul-M <ragm@redhat.com>
2023-12-07 14:34:05 +0530

    Added route_test.go

---
+++ support/route_test.go
@@ -0,0 +1,50 @@
+package support
+
+import (
+	"testing"
+    "context"
+	"github.com/onsi/gomega"
+
+	
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+
+	routev1 "github.com/openshift/api/route/v1"
+)
+
+func NewFakeKubeClientWithRoute(scheme *runtime.Scheme, objects ...client.Object) client.Client {
+	return fake.NewClientBuilder().WithScheme(scheme).WithObjects(objects...).Build()
+}
+
+func TestGetRoute(t *testing.T) {
+
+	g := gomega.NewGomegaWithT(t)
+
+	scheme := runtime.NewScheme()
+	_ = routev1.AddToScheme(scheme)
+
+	fakeroute := []client.Object{
+		&routev1.Route{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "test-1",
+				Namespace: "my-namespace",
+			},
+		},
+	}
+	fakeClient := NewFakeKubeClientWithRoute(scheme, fakeroute...)
+
+	
+    route := &routev1.Route{}
+	err := fakeClient.Get(context.TODO(), client.ObjectKey{Name: "test-1", Namespace: "my-namespace"}, route)
+	g.Expect(err).ToNot(gomega.HaveOccurred())
+	
+
+	// Assertions
+	g.Expect(route.Name).To(gomega.Equal("test-1"))
+	g.Expect(route.Namespace).To(gomega.Equal("my-namespace"))
+
+}
+
+
