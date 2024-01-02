87da9cd1c0ed57c42d09a40fd5445b69eb53f88a
Raghul-M <ragm@redhat.com>
2024-01-02 09:43:55 +0530

    Updated code

--- support/ocm_test.go
+++ support/ocm_test.go
@@ -1,21 +1,18 @@
 package support
-
 /*
+
 import (
 	"testing"
-
 	"github.com/onsi/gomega"
-
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes/fake"
-
 	ocmsdk "github.com/openshift-online/ocm-sdk-go"
 	cmv1 "github.com/openshift-online/ocm-sdk-go/clustersmgmt/v1"
 )
 
 func TestGetMachinePools(t *testing.T) {
 	// Create a fake OpenShift client for testing
-	fakeClient := fake.NewSimpleClientset(&machinePool{
+	fakeClient := fake.NewSimpleClientset(machinePool{
 		MachinePool: &cmv1.MachinePool{
 			ObjectMeta: metav1.ObjectMeta{
 				Name:      "test-machinepool",
@@ -31,8 +28,7 @@ func TestGetMachinePools(t *testing.T) {
 
 	// Call the GetMachinePools function with the fake client and connection
 	machinePools := GetMachinePools(test, &ocmsdk.Connection{})
-
 	test.Expect(machinePools).Should(gomega.HaveLen(1), "Expected 1 machine pool, but got %d", len(machinePools))
 }
-*/
 
+*/
