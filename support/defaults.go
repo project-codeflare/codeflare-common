848538ddd574eaa979f9310313ceb6866e8955cd
Fiona Waters <fiwaters6@gmail.com>
2024-09-09 16:45:51 +0100

    Upgrade Ray to version 2.35.0

--- support/defaults.go
+++ support/defaults.go
@@ -7,5 +7,5 @@ package support
 const (
 	CodeFlareSDKVersion = "0.12.0"
 	RayVersion          = "2.23.0"
-	RayImage            = "quay.io/rhoai/ray:2.23.0-py39-cu121"
+	RayImage            = "quay.io/modh/ray:2.35.0-py39-cu121"
 )
