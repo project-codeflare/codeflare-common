7d38009301c90140d7b5b965eb516e22d4c43e89
Shilpa Chugh <shchugh@redhat.com>
2024-09-16 17:50:49 +0530

    upgrade RayVersion & CodeFlareSDKVersion

--- support/defaults.go
+++ support/defaults.go
@@ -5,7 +5,7 @@ package support
 // ***********************
 
 const (
-	CodeFlareSDKVersion = "0.12.0"
-	RayVersion          = "2.23.0"
+	CodeFlareSDKVersion = "v0.20.2"
+	RayVersion          = "2.35.0"
 	RayImage            = "quay.io/modh/ray:2.35.0-py39-cu121"
 )
