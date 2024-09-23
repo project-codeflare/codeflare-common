b5ba788f9be9749c8221ff691c180b4a013cdbd0
Jiri Petrlik <jiripetrlik@gmail.com>
2024-09-23 08:55:43 +0200

    RHOAIENG-11046 - Add support for AMD GPU image

--- support/defaults.go
+++ support/defaults.go
@@ -5,6 +5,7 @@ package support
 // ***********************
 
 const (
-	RayVersion = "2.35.0"
-	RayImage   = "quay.io/modh/ray:2.35.0-py39-cu121"
+	RayVersion   = "2.35.0"
+	RayImage     = "quay.io/modh/ray:2.35.0-py39-cu121"
+	RayROCmImage = "quay.io/modh/ray:2.35.0-py39-rocm61"
 )
