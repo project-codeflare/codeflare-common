47f906f55d2ec5bea8e168b6957921c3cdd983cf
Shilpa Chugh <shchugh@redhat.com>
2024-10-03 13:56:12 +0530

    Add support for torch-amd and torch-rocm image

--- support/defaults.go
+++ support/defaults.go
@@ -5,7 +5,9 @@ package support
 // ***********************
 
 const (
-	RayVersion   = "2.35.0"
-	RayImage     = "quay.io/modh/ray:2.35.0-py39-cu121"
-	RayROCmImage = "quay.io/modh/ray:2.35.0-py39-rocm61"
+	RayVersion        = "2.35.0"
+	RayImage          = "quay.io/modh/ray:2.35.0-py39-cu121"
+	RayROCmImage      = "quay.io/modh/ray:2.35.0-py39-rocm61"
+	RayTorchCudaImage = "quay.io/rhoai/ray:2.35.0-py39-cu121-torch24-fa26"
+	RayTorchROCmImage = "quay.io/rhoai/ray:2.35.0-py39-rocm61-torch24-fa26"
 )
