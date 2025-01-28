f501cd31fe8ba625f5caebf31e92239acc433f48
Shilpa Chugh <shchugh@redhat.com>
2025-01-28 19:07:20 +0530

    Update rayROCM image

--- support/defaults.go
+++ support/defaults.go
@@ -7,7 +7,7 @@ package support
 const (
 	RayVersion        = "2.35.0"
 	RayImage          = "quay.io/modh/ray:2.35.0-py311-cu121"
-	RayROCmImage      = "quay.io/modh/ray:2.35.0-py311-rocm61"
+	RayROCmImage      = "quay.io/modh/ray:2.35.0-py311-rocm62"
 	RayTorchCudaImage = "quay.io/rhoai/ray:2.35.0-py311-cu121-torch24-fa26"
 	RayTorchROCmImage = "quay.io/rhoai/ray:2.35.0-py311-rocm61-torch24-fa26"
 	TrainingCudaImage = "quay.io/modh/training:py311-cuda121-torch241"
