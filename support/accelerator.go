8b005569586354772aebd124d5bbaeb5dbd1e53f
Shilpa Chugh <shchugh@redhat.com>
2025-03-13 15:15:59 +0530

    Move accelerator function to codeflare-common support

---
+++ support/accelerator.go
@@ -0,0 +1,18 @@
+package support
+
+var (
+	AMD    = Accelerator{Type: "gpu", ResourceLabel: "amd.com/gpu"}
+	CPU    = Accelerator{Type: "cpu"}
+	NVIDIA = Accelerator{Type: "gpu", ResourceLabel: "nvidia.com/gpu", PrometheusGpuUtilizationLabel: "DCGM_FI_DEV_GPU_UTIL"}
+)
+
+type Accelerator struct {
+	Type                          string
+	ResourceLabel                 string
+	PrometheusGpuUtilizationLabel string
+}
+
+// Method to check if the accelerator is a GPU
+func (a Accelerator) isGpu() bool {
+	return a != CPU
+}
